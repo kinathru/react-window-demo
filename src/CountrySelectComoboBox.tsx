e1c67c07b3c0b99a210367e2cf5703eac8c57eff
kinath <kinath.ru@gmail.com>
2026-02-05 15:42:35 -0600

    combobox with input change event

--- src/CountrySelectComoboBox.tsx
+++ src/CountrySelectComoboBox.tsx
@@ -8,13 +8,17 @@ import {
 } from "@/components/ui/combobox";
 
 import { Country, type ICountry } from "country-state-city";
+import { useState } from "react";
 
 const CountrySelectComboBox = () => {
-  const countries = Country.getAllCountries();
-
+  const allCountries = Country.getAllCountries();
+  const [countries, setCountries] = useState(allCountries.slice(0, 5));
   return (
     <>
       <div className="max-w-7xl mx-auto p-10">
+        <div className="flex justify-center mb-5 p-5">
+          <p>This example filters combobox values based on input</p>
+        </div>
         <div className="flex justify-center mb-5">
           <Combobox
             items={countries}
@@ -24,6 +28,19 @@ const CountrySelectComboBox = () => {
             }
             itemToStringLabel={(country: ICountry) => country.name}
             autoHighlight
+            onInputValueChange={(searchText) => {
+              if (searchText) {
+                setCountries(
+                  allCountries
+                    .slice()
+                    .filter((c) =>
+                      c.name.toLowerCase().startsWith(searchText.toLowerCase()),
+                    ),
+                );
+              } else {
+                setCountries(allCountries.slice(0, 10));
+              }
+            }}
           >
             <ComboboxInput placeholder="Select a Country" showClear />
             <ComboboxContent>
